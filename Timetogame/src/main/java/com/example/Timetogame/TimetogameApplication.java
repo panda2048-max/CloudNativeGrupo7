d743fe5b13905503cfb8c8e6e9d4599512a35df6
panda2048-max <fabr.morales@duocuc.cl>
2026-09-03 16:52:40 -0400

    iniciando creacion de proyecto

---
+++ Timetogame/src/main/java/com/example/Timetogame/TimetogameApplication.java
@@ -0,0 +1,13 @@
+package com.example.Timetogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetogameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetogameApplication.class, args);
+	}
+
+}
---
+++ Timetogame/src/test/java/com/example/Timetogame/TimetogameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Timetogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetogameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
